5b2cfe68231c816c29a7ec8c389f22e93b51d078
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-04-26 00:03:06 +0600

    fix: environment setup for hosting in cpanel

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn doctor_management_task.wsgi
--- doctor_management_task/settings.py
+++ doctor_management_task/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 MEDIA_ROOT = BASE_DIR / "media/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
